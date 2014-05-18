9970a1f98efb7e0c715f34581cccac56738d761d
davidgarcia22 <paco7295@gmail.com>
2014-05-17 20:41:11 -0600

    davids stuff

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, render_template
+#from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+
+#bootstrap = Bootstrap(app)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World! We are going to have this done!'
+def index():
+	return render_template('index.html')
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
-
-
-@app.errorhandler(500)
-def page_not_found(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name = name)
+	
+if __name__ == '__main__':
+	app.run(debug=True)
