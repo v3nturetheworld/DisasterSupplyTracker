e6b23f35b8c057567f32344fd3ca8cb8f3ffb988
Dan Cruwys <dcruwys@gmail.com>
2014-05-17 19:43:39 -0600

    deleted stupid crap

--- landing.py
+++
@@ -1,6 +0,0 @@
-import webapp2
-class main(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type']='text/plain'
-		self.response.out.write("test")
-app = webapp2.WSGIApplication([('/', main)], debug = True)
